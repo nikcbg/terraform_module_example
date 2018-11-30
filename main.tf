cf800a51ff1e184c1966aca3808f45dba53c5e34
nikcbg <43785382+nikcbg@users.noreply.github.com>
2018-11-30 15:37:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
-resource "example" "HelloWorld" {
-  provisioner "local-exec" {
-    command = "echo Hello World"
+# Configure AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Create EC2 cluster in AWS with module downloaded from Terraform Registry. 
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "1.12.0"
+
+  name                   = "my-cluster"
+  instance_count         = 5
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
